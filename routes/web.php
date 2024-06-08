6c539215998cb881cebb4ca532c56b808f29a628
kon104 <toshiharu@MacBookAir2023.local>
2024-06-08 18:24:04 +0900

    added the auth function by breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('hello', 'App\Http\Controllers\HelloController@index');
 Route::get('book', 'App\Http\Controllers\BookController@index');
@@ -13,5 +26,4 @@ Route::get('book/create', 'App\Http\Controllers\BookController@create');
 Route::put('book/{book}', 'App\Http\Controllers\BookController@update');
 Route::delete('book/{book}', 'App\Http\Controllers\BookController@destroy');
 Route::post('book', 'App\Http\Controllers\BookController@store');
-
 
